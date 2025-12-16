4303987be84cd3d334fcd379df0778eff293c81f
PawelB03 <78073907+PawelB03@users.noreply.github.com>
2025-12-16 16:08:03 +0100

    Remove JSONtoolsApplication class

--- src/main/java/pl/put/poznan/JSONtools/app/JSONtoolsApplication.java
+++ src/main/java/pl/put/poznan/JSONtools/app/JSONtoolsApplication.java
@@ -1,13 +1,1 @@
-package pl.put.poznan.JSONtools.app;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-
-@SpringBootApplication
-public class JSONtoolsApplication {
-
-    public static void main(String[] args){
-        SpringApplication.run(JSONtoolsApplication.class, args);
-    }
-}
